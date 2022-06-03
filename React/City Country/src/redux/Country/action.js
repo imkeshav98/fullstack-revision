8cc6e43c6c78c5b81967050f34b5a779d5ce041f
imkeshav98 <imkeshav98@gmail.com>
2022-06-03 12:45:17 +0530

    final

--- React/City Country/src/redux/Country/action.js
+++ React/City Country/src/redux/Country/action.js
@@ -1,25 +1,20 @@
-import axios from "axios"
+import axios from "axios";
 
+export const ADDCOUNTRY = "ADDCOUNTRY";
 
- 
-export const ADDCOUNTRY = 'ADDCOUNTRY'
+export const addCountry = (data) => {
+  return {
+    type: ADDCOUNTRY,
+    payload: data,
+  };
+};
 
-export const  addCountry = (data) => {
-    return {
-    type : ADDCOUNTRY,
-    payload : data
-    }
-}
+export const getCountry = () => async (dispatch) => {
+  axios.get("http://localhost:8080/country").then(({ data }) => {
+    dispatch(addCountry(data));
+  });
+};
 
-
-export const getCountry = () => async (dispatch)=> {
-     axios.get('http://localhost:8080/country').then(({data})=>{
-         dispatch(addCountry(data))
-     })
-}
-
-
-
-export const postCountry = (country) => async(dispatch)=> {
-       axios.post('http://localhost:8080/country',country)
-} 
+export const postCountry = (country) => async (dispatch) => {
+  axios.post("http://localhost:8080/country", country);
+};
